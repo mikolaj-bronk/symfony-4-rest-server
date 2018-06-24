6d4533492d40d8e957b5884837bb4f84a04f65d1
mikolaj-bronk <mikolaj.bronk@gmail.com>
2018-06-24 14:10:32 +0200

    add DataFixtures

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
